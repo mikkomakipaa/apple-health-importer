93934e5e9b463560c77dd17d6d2e99dae7c3674d
Mikko Mäkipää <mikko.makipaa@pm.me>
2025-08-29 18:56:13 +0300

    🔖 Checkpoint: Edit /Users/mikko.makipaa/apple_health_importer/src/apple_health_importer/parsers/__init__.py

--- src/apple_health_importer/parsers/__init__.py
+++ src/apple_health_importer/parsers/__init__.py
@@ -1,6 +1,5 @@
 """Data parsing modules."""
 
 from .health_data import HealthDataParser
-from .streaming import StreamingHealthDataProcessor
 
-__all__ = ["HealthDataParser", "StreamingHealthDataProcessor"]
+__all__ = ["HealthDataParser"]
